a7d476bf2313b4a7a86925d8e67bd746817e0b0e
Christian Bundy <me@christianbundy.com>
2014-12-03 12:48:18 -0800

    Revert "Simplify and make attack easier"
    This reverts commit bfb6ef9bc8f71684cf71275650d9fccbaded892e.

--- bombard.js
+++ bombard.js
@@ -1,6 +1,18 @@
-var bombard = function (target, quantity) {
-  if (quantity > 0) {
-    $(target).eq(0).click();
-    bombard(target, quantity - 1);
+var bombard = function (target, quantity, seconds) {
+  target   = target   || 'html';
+  quantity = quantity || 1;
+  seconds  = seconds  || 1;
+  
+  var clickEverySecond = function (element) {
+        if (quantity > 0) {
+        $(element).eq(0).click();
+        quantity--;
+        };
   };
+        
+  setInterval(function() {
+    clickEverySecond(target)
+  }, (seconds) * 1000);
 };
+
+
