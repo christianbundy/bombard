cce8472004af4f563f48c0f2e24a29a98ee742fb
Christian Bundy <christianbundy@fraction.io>
2014-12-11 16:04:45 -0800

    Use Meantime

--- bombard.js
+++ bombard.js
@@ -1,17 +1,85 @@
+// Meantime
+var mt = {
+  // Array of timers.
+  timers: [],
+
+  // Main setter function.
+  set: function (options) {
+    var self = this;
+
+    options.cb    = options.cb    || function () {};
+    options.type  = options.type  ||      'timeout';
+    options.delay = options.delay ||              0;
+    options.limit = options.limit ||       Infinity;
+
+    var setter;
+    if (options.type === 'timeout') {
+      setter = setTimeout;
+    } else if (options.type === 'interval') {
+      setter = setInterval;
+    } else {
+      return false;
+    }
+
+    var id = self.timers.length;
+
+    var timer = setter(function () {
+      if (options.limit - self.timers[id].calls++) {
+        options.cb.call(timer);
+      } else {
+        self.clear(timer);
+      }
+    }, options.delay);
+
+
+    timer.calls   =       0;
+    timer.options = options;
+    timer.clear   = function () { self.clear(timer) };
+
+    self.timers.push(timer);
+    return self.timers[self.timers.length - 1];
+  },
+
+  // Set timeout specifically.
+  setTimeout: function (options) {
+    var self = this;
+    options.type = 'timeout';
+    return self.set(options);
+  },
+
+  // Set interval speciically.
+  setInterval: function (options) {
+    var self = this;
+    options.type = 'interval';
+    return self.set(options);
+  },
+
+  // Main clear function.
+  clear: function (timer) {
+    var type = timer.options.type;
+
+    if (type === 'timeout') {
+      clearTimeout(timer);
+    } else if (type === 'interval') {
+      clearInterval(timer);
+    }
+  }
+};
+
 var bombard = function (target, quantity, delay) {
   // Use reasonable defaults for undefined arguments.
-  target   = target   || 'html';
-  quantity = quantity || 1;
-  delay    = delay    || 1000;
-  
-  var handle = setInterval(function() {
-    // Check to make element exists.
-    var el = $(target).eq(0);
-    // Click it or clear it.
-    if (el.length && quantity--) {
-      el.click();
-    } else {
-      clearInterval(handle);
-    }
-  }, delay);
+  var timer = mt.setInterval({
+    cb: function () {
+      var target = target || 'html';
+      var el = $(target).eq(0);
+      // Click it or clear it.
+      if (el.length) {
+        el.click();
+      } else {
+        this.clear();
+      }
+    },
+    limit: limit || 1,
+    delay: delay || 1000
+  });
 };
